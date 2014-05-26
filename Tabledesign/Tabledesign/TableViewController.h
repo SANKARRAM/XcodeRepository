126ba8356c6a05b2d6bfe45a39351291e3975719
SANKARRAM <annutech@annutechs-Mac-Pro-4.local>
2014-05-26 13:16:56 +0530

    TableDesign-2
    Table View Controller Added

---
+++ Tabledesign/Tabledesign/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Tabledesign
+//
+//  Created by annutech on 26/05/14.
+//  Copyright (c) 2014 annutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
